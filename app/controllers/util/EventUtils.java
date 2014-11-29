a02ae4540cef52edfec6894f6951281bffb1dd29
Dhanyatha <dmanjuna@asu.edu>
2014-11-28 18:52:20 -0700

    Update operation to mark event status as Active

--- app/controllers/util/EventUtils.java
+++ app/controllers/util/EventUtils.java
@@ -28,7 +28,7 @@ public class EventUtils {
 
     public static EventStatsWrapper EventAggregator(Event event,User user){
         //Mark the active status of the event to 2 (Completed)
-        event.updateEventStatus();
+        event.markEventStatusAsCompleted();
         //Update the event
         event.update();
         EventStatsWrapper eventStatsWrapper = new EventStatsWrapper(event);
