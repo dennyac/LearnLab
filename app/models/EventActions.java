3b2e3ffe003cbbca860428e277eaee3f8f335359
Denny Abraham Cheriyan <dennyac@gmail.com>
2014-12-01 02:36:37 -0700

    Changes to EventActions for Postgres

--- app/models/EventActions.java
+++ app/models/EventActions.java
@@ -68,7 +68,7 @@ public class EventActions extends Model{
     }
 
     public static List<EventActions> findAllUpvotesForUserByEventID(User user,Event event){
-        return find.where().eq("event", event).eq("Attribute1",user.id).eq("ActionType","UpVote").findList();
+        return find.where().eq("event", event).eq("Attribute1",user.id.toString()).eq("ActionType","UpVote").findList();
     }
    //number of informal messages
     public static int noOfInformalMessages(String EventName )
